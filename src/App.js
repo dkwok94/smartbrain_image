7b036d277536cb5471fdd23c3b05ebe52838859e
dkwok94 <dhklangs@gmail.com>
2019-02-06 01:25:05 -0800

    Add updated frontend files that link to Heroku

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({imageUrl: this.state.input})
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://smartbrain-api-dkwok94.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -73,7 +73,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://smartbrain-api-dkwok94.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
